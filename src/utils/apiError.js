efa3d789d5406255dea9604c64f2859e29ca5d4f
Githubsuer1 <mishrasiddhant560@gmail.com>
2024-12-19 21:51:52 +0530

    utils code updated

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -22,4 +22,48 @@ class APIError extends Error{
 }
 
 
-export {APIError}
+export {APIError};
+
+
+
+
+
+
+
+/*
+
+const express = require('express'); 
+const app = express(); 
+const port = process.env.PORT || 3000; 
+
+class APIError extends Error { 
+    constructor( 
+        statusCode, 
+        message = "Something went wrong!", 
+        errors = [], 
+        stack = "" 
+    ) { 
+        super(message); 
+        this.statusCode = statusCode; 
+        this.data = null; 
+        this.message = message; 
+        this.success = false; 
+        this.errors = errors; 
+        
+        if (stack) { 
+            this.stack = stack; 
+        } else { 
+            Error.captureStackTrace(this, this.constructor); 
+        } 
+    } 
+} 
+app.get('/error', (req, res) => { 
+    const error = new APIError(500, "Internal Server Error"); 
+    res.status(error.statusCode).json(error); 
+}); 
+app.listen(port, () => { 
+    console.log(`Server started on port ${port}`); 
+});
+
+
+*/
